619608241d4de462b10e42700e746402ebbdd9a6
wangchenyu <13212199211@163.com>
2020-10-18 00:19:02 +0800

    实现按一下键盘就能朝一个方向一直走，引入方向的概念

--- src/tank/TankFrame.java
+++ src/tank/TankFrame.java
@@ -10,6 +10,10 @@ public class TankFrame extends Frame {
     // 不要忘记初始化
     private int xPos = 200;
     private int yPos = 200;
+    // 引入方向的概念
+    Dir dir = Dir.DOWN;
+    // 定义坦克移动的速度
+    private static final int speed = 4;
 
 
     public TankFrame() {
@@ -28,36 +32,94 @@ public class TankFrame extends Frame {
         });
     }
 
-
     @Override
     public void paint(Graphics g) {
         g.fillRect(xPos, yPos, 50, 50);
         // 在此处使用递加的操作每次改变x，y的值来使每次调用print方法的时候使坦克动起来
+        // 这里使用方向来判断一直走的路线
+        switch (dir) {
+            case UP:
+                yPos -= speed;
+                break;
+            case DOWN:
+                yPos += speed;
+                break;
+            case LEFT:
+                xPos -= speed;
+                break;
+            case RIGHT:
+                xPos += speed;
+                break;
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
+        // 定义四个将要走的方向
+        boolean goUp = false;
+        boolean goDown = false;
+        boolean goLeft = false;
+        boolean goRight = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println(e.getKeyChar());
             int keyCode = e.getKeyCode();
             switch (keyCode) {
                 case KeyEvent.VK_UP:
-                    yPos -= 10;
+                    goUp = true;
                     break;
                 case KeyEvent.VK_DOWN:
-                    yPos += 10;
+                    goDown = true;
                     break;
                 case KeyEvent.VK_LEFT:
-                    xPos -= 10;
+                    goLeft = true;
                     break;
                 case KeyEvent.VK_RIGHT:
-                    xPos += 10;
+                    goRight = true;
+                    break;
+                default:
                     break;
             }
+            // 更新坦克行走的方向
+            upTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+                    goUp = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    goDown = false;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    goLeft = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    goRight = false;
+                    break;
+                default:
+                    break;
+            }
+            // 更新坦克行走的方向
+            upTankDir();
+        }
+
+        private void upTankDir() {
+            // 如果键盘按住了，则会将方向设置成对应的方向，如果释放了，则不对但钱方向进行修改
+            if (goLeft) {
+                dir = Dir.LEFT;
+            }
+            if (goRight) {
+                dir = Dir.RIGHT;
+            }
+            if (goUp) {
+                dir = Dir.UP;
+            }
+            if (goDown) {
+                dir = Dir.DOWN;
+            }
         }
     }
 }
