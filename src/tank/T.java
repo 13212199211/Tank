fe7bd56a637c80084412a6bf8b3e3f2d0e7fc875
wangchenyu <13212199211@163.com>
2020-10-17 23:01:39 +0800

    从Frame类继承，并重写print方法，画出一个黑方块

--- src/tank/T.java
+++ src/tank/T.java
@@ -1,22 +1,7 @@
 package tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class T {
     public static void main(String[] args) {
-        Frame frame = new Frame();
-        frame.setSize(800, 600);
-        frame.setTitle("tank war");
-        frame.setResizable(false);
-        frame.setVisible(true);
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame tankFrame = new TankFrame();
     }
 }
